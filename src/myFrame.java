ca9cc1d92d17e7eb5ca93b2ef7ff8f7d74aa749b
homuhomy <74242263+homuhomy@users.noreply.github.com>
2021-12-19 18:28:04 +0800

    completed

--- src/myFrame.java
+++ src/myFrame.java
@@ -8,8 +8,14 @@ public class myFrame extends JFrame {
 
     Calendar calendar;
     SimpleDateFormat timeFormat;
+    SimpleDateFormat dayFormat;
+    SimpleDateFormat dateFormat;
     JLabel timeLabel;
+    JLabel dayLabel;
+    JLabel dateLabel;
     String time;
+    String day;
+    String date;
 
     myFrame(){
 
@@ -22,19 +28,33 @@ public class myFrame extends JFrame {
         this.getContentPane().setBackground(new Color(255,252,220));
 
         //https://docs.oracle.com/javase/7/docs/api/java/text/SimpleDateFormat.html#text
+
+        //set Time
         timeFormat = new SimpleDateFormat("hh:mm:ss a");
+        //set Day
+        dayFormat = new SimpleDateFormat("EEEE");
+        //set date
+        dateFormat = new SimpleDateFormat("MMMMM || dd/MM/yy");
 
         timeLabel = new JLabel();
-        timeLabel.setFont(new Font("Verdana", Font.PLAIN,50)); //change font (plain is font style)
+        timeLabel.setFont(new Font("Verdana", Font.ITALIC,50)); //change font (plain is font style)
         timeLabel.setForeground(new Color(105,152,171)); //change text color
         //timeLabel.setBackground(new Color(81,107,235));
         //setOpaque(true)
 
-        time = timeFormat.format(Calendar.getInstance().getTime()); //get current date and time
-        timeLabel.setText(time);
+        dayLabel = new JLabel();
+        dayLabel.setFont(new Font("Verdana", Font.PLAIN,25)); //change font (plain is font style)
+        dayLabel.setForeground(new Color(105,152,171)); //change text color
+
+        dateLabel = new JLabel();
+        dateLabel.setFont(new Font("Verdana", Font.ITALIC,25));
+        dateLabel.setForeground(Color.BLACK);
 
         this.add(timeLabel);
+        this.add(dayLabel);
+        this.add(dateLabel);
         this.setVisible(true);
+
 
         setTime(); //to make the clock update time every second
 
@@ -44,6 +64,12 @@ public class myFrame extends JFrame {
         while(true){
             time = timeFormat.format(Calendar.getInstance().getTime()); //get current date and time
             timeLabel.setText(time);
+
+            day = dayFormat.format(Calendar.getInstance().getTime()); //get current date and time
+            dayLabel.setText(day);
+
+            date = dateFormat.format(Calendar.getInstance().getTime()); //get current date and time
+            dateLabel.setText(date);
 
             //only update every second
             try {
